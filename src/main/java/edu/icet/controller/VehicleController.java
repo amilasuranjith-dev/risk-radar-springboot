36b410a9a3a317b8e3faf7e393648c14553f7a0c
amilasuranjith-dev <amilasuranjith.dev@gmail.com>
2026-06-15 22:57:00 +0530

    chore: standardize controller responses and global error handling

--- src/main/java/edu/icet/controller/VehicleController.java
+++ src/main/java/edu/icet/controller/VehicleController.java
@@ -4,6 +4,7 @@ import edu.icet.dto.VehicleRequestDto;
 import edu.icet.dto.VehicleResponseDto;
 import edu.icet.service.VehicleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,23 +17,23 @@ public class VehicleController {
     private final VehicleService vehicleService;
 
     @GetMapping("/get-all")
-    public List<VehicleResponseDto> getAll() {
-        return vehicleService.getAll();
+    public ResponseEntity<List<VehicleResponseDto>> getAll() {
+        return ResponseEntity.ok(vehicleService.getAll());
     }
 
     @PostMapping("/add")
-    public boolean add(@RequestBody VehicleRequestDto vehicleRequest) {
-        return vehicleService.add(vehicleRequest);
+    public ResponseEntity<Boolean> add(@RequestBody VehicleRequestDto vehicleRequest) {
+        return ResponseEntity.status(org.springframework.http.HttpStatus.CREATED).body(vehicleService.add(vehicleRequest));
     }
 
     @PutMapping("/update/{id}")
-    public boolean update(@PathVariable Long id, @RequestBody VehicleRequestDto vehicleRequest) {
-        return vehicleService.update(id, vehicleRequest);
+    public ResponseEntity<Boolean> update(@PathVariable Long id, @RequestBody VehicleRequestDto vehicleRequest) {
+        return ResponseEntity.ok(vehicleService.update(id, vehicleRequest));
     }
 
     @DeleteMapping("/deleteById/{id}")
-    public boolean deleteById(@PathVariable Long id) {
-        return vehicleService.deleteById(id);
+    public ResponseEntity<Boolean> deleteById(@PathVariable Long id) {
+        return ResponseEntity.ok(vehicleService.deleteById(id));
     }
 
     @GetMapping("/searchById/{id}")
